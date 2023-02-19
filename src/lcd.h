c03f246b4e2b03fd1b46cfd677f8e166af9b2097
Ruedi Heimlicher <r.heimlicher@bluewin.ch>
2023-02-19 12:51:39 +0100

    lcd-funktionen in externes file

--- src/lcd.h
+++ src/lcd.h
@@ -8,5 +8,14 @@
 #include <stdint.h>
 #include <LiquidCrystal_I2C.h>
 
+extern LiquidCrystal_I2C lcd;
 
+void lcd_putc(const char c);
 void lcd_puts(const char *s);
+void lcd_putint(uint8_t zahl);
+void lcd_putint12(uint16_t zahl);
+void lcd_putint16(uint16_t zahl);
+void lcd_putint2(uint8_t zahl);	//zweistellige Zahl
+void lcd_putint1(uint8_t zahl);	//einstellige Zahl
+void lcd_puthex(uint8_t zahl);
+void lcd_putint999(uint16_t zahl);
